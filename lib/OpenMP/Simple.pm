cbfb4a9d486552635308f3d2bfc3186eeeaa51d3
O. Odler, 577 <oodler@cpan.org>
2023-07-24 03:01:21 +0000

    Adding final tests for runtime MACROS (for now), updated POD.

--- lib/OpenMP/Simple.pm
+++ lib/OpenMP/Simple.pm
@@ -11,39 +11,44 @@ sub Inline {
   my $config = Alien::OpenMP->Inline($lang);
   $config->{AUTO_INCLUDE} .=q{
 
-/* %ENV Update Macros (doxygen style comments) */
-
 /**
 All setters EXCEPT the LOCK retoutines,
   as defined at https://gcc.gnu.org/onlinedocs/libgomp/Runtime-Library-Routines.html
 
-   Status                            GCC*     Description
- * DONE   omp_set_num_threads        8.3.0  – Set upper team size limit
- * DONE   omp_set_schedule           8.3.0  – Set the runtime scheduling method
- * DONE   omp_set_dynamic            8.3.0  – Enable/disable dynamic teams
- * DONE   omp_set_nested             8.3.0  – Enable/disable nested parallel regions
- * DONE   omp_set_max_active_levels  8.3.0  – Limits the number of active parallel regions
- * DONE   omp_set_num_teams         12.3.0  – Set upper teams limit for teams construct
- * wip  omp_set_teams_thread_limit – Set upper thread limit for teams construct
- *   omp_set_default_device – Set the default device for target regions
+   Status                              GCC*     Description
++-------------------------------------------------------------------------------------------+
+ - DONE   omp_set_num_threads          8.3.0  – Set upper team size limit
+ - DONE   omp_set_schedule             8.3.0  – Set the runtime scheduling method
+ - DONE   omp_set_dynamic              8.3.0  – Enable/disable dynamic teams
+ - DONE   omp_set_nested               8.3.0  – Enable/disable nested parallel regions
+ - DONE   omp_set_max_active_levels    8.3.0  – Limits the number of active parallel regions
+ - DONE   omp_set_default_device       8.3.0 – Set the default device for target regions
+ - DONE   omp_set_num_teams           12.3.0  – Set upper teams limit for teams construct
+ - DONE   omp_set_teams_thread_limit  12.3.0 – Set upper thread limit for teams construct
++-------------------------------------------------------------------------------------------+
 **/
+
+/* %ENV Update Macros */
+
+#define PerlOMP_ENV_SET_DEFAULT_DEVICE                      \
+    char *num = getenv("OMP_DEFAULT_DEVICE");               \
+    omp_set_default_device(atoi(num));                      ///< read and update with $ENV{OMP_NUM_TEAMS}
 
 #define PerlOMP_ENV_SET_TEAMS_THREAD_LIMIT                  \
     char *num = getenv("OMP_TEAMS_THREAD_LIMIT");           \
-    omp_set_teams_thread_limit(atoi(num));            ///< read and update with $ENV{OMP_NUM_TEAMS}
-
+    omp_set_teams_thread_limit(atoi(num));                  ///< read and update with $ENV{OMP_NUM_TEAMS}
 
 #define PerlOMP_ENV_SET_NUM_TEAMS                           \
     char *num = getenv("OMP_NUM_TEAMS");                    \
-    omp_set_num_teams(atoi(num));                    ///< read and update with $ENV{OMP_NUM_TEAMS}
+    omp_set_num_teams(atoi(num));                           ///< read and update with $ENV{OMP_NUM_TEAMS}
 
 #define PerlOMP_ENV_SET_MAX_ACTIVE_LEVELS                   \
     char *num = getenv("OMP_MAX_ACTIVE_LEVELS");            \
-    omp_set_max_active_levels(atoi(num));            ///< read and update with $ENV{OMP_MAX_ACTIVE_LEVELS}
+    omp_set_max_active_levels(atoi(num));                   ///< read and update with $ENV{OMP_MAX_ACTIVE_LEVELS}
 
 #define PerlOMP_ENV_SET_NUM_THREADS                         \
     char *num = getenv("OMP_NUM_THREADS");                  \
-    omp_set_num_threads(atoi(num));                  ///< read and update with $ENV{OMP_NUM_THREADS}
+    omp_set_num_threads(atoi(num));                         ///< read and update with $ENV{OMP_NUM_THREADS}
 
 #define PerlOMP_ENV_SET_DYNAMIC                             \
     char *VALUE = getenv("OMP_DYNAMIC");                    \
@@ -55,7 +60,7 @@ All setters EXCEPT the LOCK retoutines,
     }                                                       \
     else {                                                  \
       omp_set_dynamic(NULL);                                \
-    };                                               ///> read and update with $ENV{OMP_DYNAMIC} 
+    };                                                      ///> read and update with $ENV{OMP_DYNAMIC} 
 
 #define PerlOMP_ENV_SET_NESTED                              \
     char *VALUE = getenv("OMP_NESTED");                     \
@@ -67,30 +72,30 @@ All setters EXCEPT the LOCK retoutines,
     }                                                       \
     else {                                                  \
       omp_set_nested(NULL);                                 \
-    };                                        ///> read and update with $ENV{OMP_NESTED} 
+    };                                                      ///> read and update with $ENV{OMP_NESTED} 
 
-#define PerlOMP_ENV_SET_SCHEDULE              \
-    char *str = getenv("OMP_SCHEDULE");       \
-    omp_sched_t SCHEDULE = omp_sched_static;  \
-    int CHUNK = 1; char *pt;                  \
-    pt = strtok (str,",");                    \
-    if (strcmp(pt,"static")) {                \
-      SCHEDULE = omp_sched_static;            \
-    }                                         \
-    else if (strcmp(pt,"dynamic")) {          \
-      SCHEDULE = omp_sched_dynamic;           \
-    }                                         \
-    else if (strcmp(pt,"guided")) {           \
-      SCHEDULE = omp_sched_guided;            \
-    }                                         \
-    else if (strcmp(pt,"auto")) {             \
-      SCHEDULE = omp_sched_auto;              \
-    }                                         \
-    pt = strtok (NULL, ",");                  \
-    if (pt != NULL) {                         \
-      CHUNK = atoi(pt);                       \
-    }                                         \
-    omp_set_schedule(SCHEDULE, CHUNK);        ///< read and update with $ENV{OMP_SCHEDULE}
+#define PerlOMP_ENV_SET_SCHEDULE                            \
+    char *str = getenv("OMP_SCHEDULE");                     \
+    omp_sched_t SCHEDULE = omp_sched_static;                \
+    int CHUNK = 1; char *pt;                                \
+    pt = strtok (str,",");                                  \
+    if (strcmp(pt,"static")) {                              \
+      SCHEDULE = omp_sched_static;                          \
+    }                                                       \
+    else if (strcmp(pt,"dynamic")) {                        \
+      SCHEDULE = omp_sched_dynamic;                         \
+    }                                                       \
+    else if (strcmp(pt,"guided")) {                         \
+      SCHEDULE = omp_sched_guided;                          \
+    }                                                       \
+    else if (strcmp(pt,"auto")) {                           \
+      SCHEDULE = omp_sched_auto;                            \
+    }                                                       \
+    pt = strtok (NULL, ",");                                \
+    if (pt != NULL) {                                       \
+      CHUNK = atoi(pt);                                     \
+    }                                                       \
+    omp_set_schedule(SCHEDULE, CHUNK);                      ///< read and update with $ENV{OMP_SCHEDULE}
 
 // ... add all of them from OpenMP::Environment, add unit tests
 
@@ -212,13 +217,66 @@ environmental variable.
 
 =head1 PROVIDED MACROS
 
+=head2 Updating Runtime with Environmental Variables
+
+When C<Inline::C> compiles and loads these shared libraries once, when the script is first loaded. At
+this time, the OpenMP runtime inspects C<%ENV>; but the environment is not automatically updated as
+whenever the runtime interfaces to the compiled library is used. The following macros offer a very
+concise and convenient way to update in the runtime based on the values of the respective environmental
+variables. They are designed with the use of C<OpenMP::Environment> in mind, which provides an OpenMP
+specific OOP interface to getting and setting these variables. Nearly all of them correspond to a way
+to C<set> them in the OpenMP runtime (provided by the compiler's implementation, e.g., C<omp.h>). 
+
+All MACROS have at least 1 test. Please those in the Github repository to get an idea of how to use
+C<OpenMP::Simple>'s macros with C<OpenMP::Environment>.
+
 =over 4
 
 =item C<PerlOMP_ENV_SET_NUM_THREADS>
 
-When used, the value of C<OMP_NUM_THREADS> will be read and be used to update with the runtime the
-number of threads via OpenMP standard runtime function, C<omp_set_num_threads> (as implemented by
-GCC's GOMP).
+Updates the OpenMP runtime with the value of the environmental variable, C<$ENV{OMP_NUM_THREADS}>,
+which is managed via C<< OpenMP::Environment->omp_num_threads[int numThreads]); >>.
+
+=item C<PerlOMP_ENV_SET_DEFAULT_DEVICE>
+
+Updates the OpenMP runtime with the value of the environmental variable, C<$ENV{OMP_DEFAULT_DEVICE}>,
+which is managed via C<< OpenMP::Environment->omp_default_device([int deviceNo]); >>.
+
+=item C<PerlOMP_ENV_SET_MAX_ACTIVE_LEVELS>
+
+Updates the OpenMP runtime with the value of the environmental variable, C<$ENV{OMP_MAX_ACTIVE_LEVELS}>,
+which is managed via C<< OpenMP::Environment->omp_max_active_levels([int maxLevel]); >>.
+
+=item C<PerlOMP_ENV_SET_DYNAMIC>
+
+Updates the OpenMP runtime with the value of the environmental variable, C<$ENV{OMP_DYNAMIC}>,
+which is managed via C<< OpenMP::Environment->omp_dynamic(['true'|'false']); >>.
+
+=item C<PerlOMP_ENV_SET_NESTED>
+
+Updates the OpenMP runtime with the value of the environmental variable, C<$ENV{OMP_NESTED}>,
+which is managed via C<< OpenMP::Environment->omp_nested(['true'|'false']); >>.
+
+=item C<PerlOMP_ENV_SET_SCHEDULE>
+
+Updates the OpenMP runtime with the value of the environmental variable, C<$ENV{OMP_SCHEDULE}>,
+which is managed via C<< OpenMP::Environment->omp_schedule(...); >>.
+
+Note: The schedule syntax is of the form I<schedule[;chunkSize]>.
+
+=item C<PerlOMP_ENV_SET_TEAMS_THREAD_LIMIT>
+
+Updates the OpenMP runtime with the value of the environmental variable, C<$ENV{OMP_TEAMS_THREAD_LIMIT}>,
+which is managed via C<< OpenMP::Environment->omp_([int limit]); >>.
+
+Note: Not supported until GCC 12.3.0
+
+=item C<PerlOMP_ENV_SET_NUM_TEAMS>
+
+Updates the OpenMP runtime with the value of the environmental variable, C<$ENV{OMP_NUM_TEAMS}>,
+which is managed via C<< OpenMP::Environment->omp_([int num]); >>.
+
+Note: Not supported until GCC 12.3.0
 
 =item C<PerlOMP_RET_ARRAY_REF_ret>
 
