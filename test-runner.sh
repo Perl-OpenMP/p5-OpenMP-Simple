ffbf4d4d06ea2893a2311f8cbd7744c725888f2a
O. Odler, 577 <oodler@cpan.org>
2025-03-10 23:42:31 +0000

    playing with test runner

--- test-runner.sh
+++ test-runner.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-mkdir -p blib/lib/auto/share/dist/OpenMP-Simple/ > /dev/null 2>&1
+rm -rf blib > /dev/null 2>&1
+mkdir -p blib/lib/auto/share/dist/OpenMP-Simple/
 cp -f share/openmp-simple.h blib/lib/auto/share/dist/OpenMP-Simple/openmp-simple.h
 cp -f share/ppport.h blib/lib/auto/share/dist/OpenMP-Simple/ppport.h
 PERL_DL_NONLAZY=1 perl -Ilib -MExtUtils::Command::MM -MTest::Harness -e "undef *Test::Harness::Switches; test_harness(1, 'blib/lib', 'blib/arch')" t/*.t | grep '\.\.\.\.'
